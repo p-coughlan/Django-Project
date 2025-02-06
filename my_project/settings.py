22876292f038c6db3f1bb1f6b29a99b3994e8025
Patrick Coughlan <patrickcoughlan@yahoo.co.uk>
2025-02-06 14:53:25 +0000

    Add about app with initial configuration and models

--- my_project/settings.py
+++ my_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello_world', # Add the hello_world app to the INSTALLED_APPS list.
+    'about', # Add the about app to the INSTALLED_APPS list.
 ]
 
 MIDDLEWARE = [
